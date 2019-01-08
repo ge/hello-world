45534f8de4195f721573ac0168387cf1b6e0b734
George Entenman <zabouti@gmail.com>
2019-01-08 05:55:30 -0500

    Added component with arg
    The Header component can now take an argument specifying the text of the header.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `purple` }}>
-	<Header />
+	<Header HeaderText="This is Strange" />
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />  </div>
 )
